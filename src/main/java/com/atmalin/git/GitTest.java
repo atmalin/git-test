625338a3e70230510db4b986d6a6c9bcb88f91a1
MaLin <ml2907957301@163.com>
2022-06-17 16:22:03 +0800

    first commit

---
+++ src/main/java/com/atmalin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atmalin.git;
+
+/**
+ * @author malin
+ * @create 2022-06-17-16:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
